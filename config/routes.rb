0fdb358dbd308b2d19eed990da9ed5d9b32364c3
Yoshifumi Nishimura <nicchi0307@gmail.com>
2021-02-10 01:30:00 +0900

    home, index画面, header, footer作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'static_pages#home'
-  get 'static_pages/home'
+  get '/index', to: 'static_pages#index'
+  get '/signup', to: 'users#new'
 end
