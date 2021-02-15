dd1f6362e7e2f914378259c35fa67cdf85ac7e31
Yoshifumi Nishimura <nicchi0307@gmail.com>
2021-02-15 15:59:04 +0900

    heroku re-test

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'users/new'
+  root 'static_pages#home'
+  get '/index', to: 'static_pages#index'
+  get '/signup', to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  resources :users
+end
